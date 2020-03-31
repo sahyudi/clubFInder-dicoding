ce58b030baa4c1a115c75a06935f5bbea2651195
sahyudi <poetrageranting@gmail.com>
2020-03-31 15:15:29 +0700

    merubah function menggunakan arrow function

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -3,14 +3,14 @@ const main = function () {
     const buttonSearchElement = document.querySelector("#searchButtonElement");
     const clubListElement = document.querySelector("#clubList");
 
-    const onButtonSearchClicked = function () {
+    const onButtonSearchClicked = () => {
         const dataSource = new DataSource(renderResult, fallbackResult);
         dataSource.searchClub(searchElement.value);
     };
 
-    const renderResult = function (results) {
+    const renderResult = results => {
         clubListElement.innerHTML = "";
-        results.forEach(function (club) {
+        results.forEach(club => {
             // const name = club.name;
             // const fanArt = club.fanArt;
             // const description = club.description;
@@ -29,7 +29,7 @@ const main = function () {
         })
     };
 
-    const fallbackResult = function (message) {
+    const fallbackResult = message => {
         clubListElement.innerHTML = "";
         clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`
     };
